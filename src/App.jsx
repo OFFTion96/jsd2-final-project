b848a12679cdd7c9f4456d26aef77727dafb76f3
Bigorian <s.wittavat@gmail.com>
2022-08-09 01:19:59 +0700

    Edite Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+
 import Navbar from './components/Navbar/Navbar'
 import './App.css'
 
 function App() {
 
+
 return (
     <div className="App">
-      <Navbar />
+      
+      <ProfilePage />
+    
     </div>
   )
 }
